4bf21b86b045e30ab1e87cdf7c1d6841e53bc1e6
Priyanshu Das <priyanshudas01@gmail.com>
2024-02-18 19:19:49 +0530

    Nav section added
    added nav component section

---
+++ Landing_page/validate/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Nav extends PureComponent {
+  render() {
+    return (
+      <div>
+        hh
+      </div>
+    )
+  }
+}
+
+export default Nav
