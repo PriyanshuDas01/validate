d1b0e86be991fb9fc124a30a128c77a63dcabc28
Priyanshu Das <priyanshudas01@gmail.com>
2024-08-23 01:57:57 +0530

    front elements' position fixed

--- Landing_page/validate/src/components/Front.jsx
+++ Landing_page/validate/src/components/Front.jsx
@@ -10,10 +10,10 @@ export class Front extends PureComponent {
       
           <div>
           <img src={Front_image} alt="Front" className="w-full h-[70vh] md:h-full object-cover" />
-            <div className=" absolute z-20 top-[20vh] ml-[13vh] md:mt-[8vh] md:ml-[27vh]">
-              <img src={icon} alt="icon" className=" md:h-[30vh]" />
+            <div className=" absolute z-20 top-[20vh] left-0 right-0 flex justify-center items-center md:mt-[4vh] md:ml-[20vh] md:justify-start md:items-start ">
+              <img src={icon} alt="icon" className=" md:h-[40vh]" />
             </div>
-            <div className="absolute z-30 top-[30vh] left-0 right-0 flex justify-center items-center mt-[15vh] md:mt-[35vh]">
+            <div className="absolute z-30 top-[30vh] left-0 right-0 flex justify-center items-center mt-[15vh] md:mt-[45vh]">
               <button className=" bg-fuchsia-900 rounded-lg text-fuchsia-400 pl-4 pr-4 pt-2 pb-2 md:text-2xl md:pt-3 md:pb-3 md:pl-8 md:pr-8   ">Add to Chrome for FREE!!</button>
               </div>
           </div>
